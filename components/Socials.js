46d8a353ce32506e4e65a2851fd5a1029e8b6eef
Edmund <edmundjet99@gmail.com>
2024-03-10 16:48:12 -0700

    Circles added to about page, change size of logo

--- components/Socials.js
+++ components/Socials.js
@@ -13,9 +13,24 @@ import {
 
 const Socials = () => {
   return (
-    <div>
-      <Link href={""} className="">
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookBoxLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
         <RiYoutubeLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiDribbbleLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiBehanceLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiPinterestLine />
       </Link>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -40,10 +40,7 @@ const Home = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            aliquip ex ea commodo consequat.
           </motion.p>
           {/* Buttons */}
           <div className="flex justify-center xl:hidden relative">
@@ -65,7 +62,7 @@ const Home = () => {
         {/* Background_Image */}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
         {/* Paticles */}
-        {/* <ParticlesContainer /> */}
+        <ParticlesContainer />
         {/* Avatar Image */}
         <motion.div
           variants={fadeIn("up", 0.5)}
