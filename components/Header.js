ddc8a8ed877d5ffd52ddb1feae9f806534186013
Edmund <edmundjet99@gmail.com>
2024-03-10 17:38:40 -0700

    about page details UI

--- components/Header.js
+++ components/Header.js
@@ -11,13 +11,13 @@ const Header = () => {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-2">
           {/* Logo */}
           <Link href={"/"}>
             <Image
               src={"/ed-logo.png"}
-              width={150}
-              height={20}
+              width={100}
+              height={100}
               alt="logo"
               priority={true}
             />
